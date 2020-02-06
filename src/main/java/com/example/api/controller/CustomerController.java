9e91146ce39a01d0e5b0760100dd14e4f99b3d7e
Samuel de Souza <48731057+samueldesouzadev@users.noreply.github.com>
2020-02-06 18:00:37 -0300

    commit

--- src/main/java/com/example/api/controller/CustomerController.java
+++ src/main/java/com/example/api/controller/CustomerController.java
@@ -37,13 +37,13 @@ public class CustomerController {
         return service.findAll();
     }
 
-    @GetMapping(UriMappingConstants.Customer.FINDALL)
+    @GetMapping(UriMappingConstants.Standard.FINDALL)
     @ApiOperation(value = "Returns all customers")
     public List<Customer> findAll(@RequestParam int page, int pageSize) {
         return service.findAll(page, pageSize);
     }
 
-    @GetMapping(UriMappingConstants.Customer.FINDBYID)
+    @GetMapping(UriMappingConstants.Standard.FINDBYID)
     @ApiOperation(value = "Returns a unique customer")
     public Customer findById(@PathVariable Long id) {
         return service.findById(id)
@@ -51,37 +51,37 @@ public class CustomerController {
                         MessageMappingConstants.Customer.CUSTOMER_NOT_FOUND));
     }
 
-    @RequestMapping(value = UriMappingConstants.Customer.SAVE, method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE)
+    @RequestMapping(value = UriMappingConstants.Standard.SAVE, method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE)
     @ResponseBody
     @ApiOperation(value = "Save a customer")
     public ResponseEntity save(@RequestBody Customer customer) {
-        try{
+        try {
             service.save(customer);
             return new ResponseEntity("Cliente salvo com sucesso", HttpStatus.OK);
-        }catch (Exception e){
+        } catch (Exception e) {
             return new ResponseEntity("Erro ao salvar cliente.", HttpStatus.BAD_REQUEST);
         }
 
     }
 
-    @PutMapping(UriMappingConstants.Customer.EDIT)
+    @PutMapping(UriMappingConstants.Standard.EDIT)
     @ApiOperation(value = "Edit a customer")
     public ResponseEntity edit(@RequestBody Customer customer) {
-        try{
+        try {
             service.save(customer);
             return new ResponseEntity("Cliente foi editado com sucesso", HttpStatus.OK);
-        }catch (Exception e){
+        } catch (Exception e) {
             return new ResponseEntity("Erro ao editar cliente.", HttpStatus.BAD_REQUEST);
         }
     }
 
-    @DeleteMapping(UriMappingConstants.Customer.DELETE)
+    @DeleteMapping(UriMappingConstants.Standard.DELETE)
     @ApiOperation(value = "Delete a customer")
     public ResponseEntity delete(@RequestBody Customer customer) {
-        try{
+        try {
             service.delete(customer);
             return new ResponseEntity("Cliente deletado com sucesso", HttpStatus.OK);
-        }catch (Exception e){
+        } catch (Exception e) {
             return new ResponseEntity("Erro ao deletado cliente.", HttpStatus.BAD_REQUEST);
         }
 
--- src/main/java/com/example/api/mapping/UriMappingConstants.java
+++ src/main/java/com/example/api/mapping/UriMappingConstants.java
@@ -6,7 +6,7 @@ public class UriMappingConstants {
         public static final String CUSTOMERS = "customer";
     }
 
-    public static final class Customer{
+    public static final class Standard{
         public static final String SAVE = "/save";
         public static final String FINDBYID = "/{id}";
         public static final String FINDALL = "/findAll";
